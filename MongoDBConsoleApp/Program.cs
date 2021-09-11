48a0e31268371bff54b0910f07693c648287a70d
JosephWee <joseph.wee.tze.yeong@gmail.com>
2021-09-11 17:39:29 +0800

    Commit-2021-09-11-1737 Add code for creating a DB "MyFirstMongoDB" and Collection "Products" Add code for inserting some documents into Collection "Products"

--- MongoDBConsoleApp/Program.cs
+++ MongoDBConsoleApp/Program.cs
@@ -3,13 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver.Linq;
 
 namespace MongoDBConsoleApp
 {
     class Program
     {
+        public class Product
+        {
+            public ObjectId Id { get; set; }
+            public int prod_id { get; set; }
+            public double price { get; set; }
+            public DateTime entryDateTime { get; set; }
+        }
+
         static void Main(string[] args)
         {
+            var connString =
+                System
+                .Configuration
+                .ConfigurationManager
+                .ConnectionStrings["MongoDB"];
+
+            MongoClient dbClient = new MongoClient(connString.ConnectionString);
+
+            var db = dbClient.GetDatabase("MyFirstMongoDB");
+
+            UpdateProductPrice(db);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void UpdateProductPrice(IMongoDatabase db)
+        {
+            Random rand = new Random();
+
+            var products = db.GetCollection<Product>("Products");
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                var product = new Product()
+                {
+                    prod_id = i,
+                    price = 12 + ((double)rand.Next(-5, 5) / 10D),
+                    entryDateTime = DateTime.UtcNow
+                };
+
+                products.InsertOne(product);
+            }
         }
     }
 }
